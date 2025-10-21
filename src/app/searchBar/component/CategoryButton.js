86ed549a268567e968e8eb76706842bb166951b3
moonwonkim-a <happynei200@gmail.com>
2025-10-21 16:40:16 +0900

    feat: searchBar 추가

--- src/app/searchBar/component/CategoryButton.js
+++ src/app/searchBar/component/CategoryButton.js
@@ -3,12 +3,11 @@ export default function CategoryButton({ onSelectCategory }) {
     const categories = ['all', '치킨', '피자', '한식']; 
 
     return (
-        <div style={{ marginTop: '10px' }}>
+        <div>
             {categories.map(category => (
                 <button 
                     key={category}
-                    onClick={() => onSelectCategory(category)} 
-                    style={{ marginRight: '5px' }}
+                    onClick={() => onSelectCategory(category)}
                 >
                     {category}
                 </button>
