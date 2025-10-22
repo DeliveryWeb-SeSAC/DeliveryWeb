ac29ffe991604826d204b1a32b2f7fcec88bac61
moonwonkim-a <happynei200@gmail.com>
2025-10-23 01:28:10 +0900

    feat: storeItem css, store css 추가

--- src/app/store/page.js
+++ src/app/store/page.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import StoreItem from '@/app/store/storeItem/StoreItem';
 import SearchBar from '../searchBar/page';
 import { useRouter } from 'next/navigation';
+import styles from '@/app/store/store.module.css'; // CSS 모듈 import
 
 export default function Store() {
   const [stores, setStores] = useState([]);
@@ -54,7 +55,8 @@ export default function Store() {
   return (
     <>
       <SearchBar onSearch={handleSearch} onFilter={handleFilter} />
-      <div>
+      {/* storeContainer 클래스를 적용합니다. */}
+      <div className={styles.storeContainer}> 
         {newList.length > 0 ? (
           newList.map((store) => (
             <StoreItem key={store.id} store={store} onClick={()=>goDetailPage(store.id)}/>
