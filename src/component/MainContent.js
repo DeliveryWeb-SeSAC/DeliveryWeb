dfc74cdafd40bfa4ccf1294c5676488eda8a44a6
soy <teabag759@gmail.com>
2025-10-22 16:48:03 +0900

    resolve

---
+++ src/component/MainContent.js
@@ -0,0 +1,11 @@
+import StorePage from '@/app/store/page'
+
+export default function MainContent({children}) {
+    return (
+        <>
+            <div>
+                <StorePage/>
+            </div>
+        </>
+    )
+}
