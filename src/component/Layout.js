bebab1990038c3a6923f35e477da6fad50d2cb5d
soy <teabag759@gmail.com>
2025-10-22 13:22:30 +0900

    style: 로그인 성공 페이지 변경

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,20 +1,27 @@
+"use client"
 import Header from './Header'
 import LoginPage from '@/app/login/page'
 import ChatPage from '@/app/chat/page'
 import SNSListPage from '@/app/msg/page'
 import CartPage from '@/app/cart/page'
+import Store from '@/app/store/page'
+import { usePathname } from 'next/navigation'
+
 
 export default function Layout({children}){
+    const pathname = usePathname()
+    const isLoginSuccess = pathname?.includes('/login/success')
     return (
         <>
             <Header/>
             <div style={{display: 'flex'}}>
                 <div style={{flex: 1}}>
-                    <LoginPage/>
+                    {isLoginSuccess ? children : <LoginPage/>}
                     <SNSListPage/>
                 </div>
                 <div style={{flex: 2}}>
-                    {children}
+                    {/* {!isLoginSuccess && children} */}
+                    <Store/>
                 </div>
                 <div style={{flex: 1}}>
                     <ChatPage/>
